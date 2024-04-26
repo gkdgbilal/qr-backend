99a5cf195388f0fe8f2672bfe149ecb2a254ebf7
Bilal Gokdag <bilal.gkdg@gmail.com>
2024-04-26 16:10:53 +0300

    feature: - Added allowed origin urls

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,4 +2,7 @@ export const allowedOrigins = [
   'https://www.yoursite.com',
   'http://127.0.0.1:8000',
   'http://localhost:8000',
+  'https://qr-backend-sand.vercel.app',
+  'https://qr-backend-git-main-gkdgbilals-projects.vercel.app',
+  'https://qr-backend-nno1woprv-gkdgbilals-projects.vercel.app',
 ];
